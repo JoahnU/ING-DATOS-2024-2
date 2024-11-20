54f1386fcc9dd6831b8385426b5845731179825a
Juan David Castañeda <jdcb30112005@gmail.com>
2024-11-20 16:28:43 -0500

    Ejemplo de insercion con sqlachemy

--- App/operacionesDB.py
+++ App/operacionesDB.py
@@ -1,2 +1,15 @@
-import models 
+from models import Jugador, engine
 
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def registrarUsuario(name, email, password):
+    nuevoJugador = Jugador(
+        user_name = name, 
+        email_adress = email
+
+    )
+    session.add(nuevoJugador)
+    return session.commit()
